c6134c2070c879ca0481ccbf239f7554362f7776
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-06-21 10:54:53 -0500

    Added Features to App

--- client/App.js
+++ client/App.js
@@ -4,11 +4,14 @@ import React from 'react';
 //  Children components
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
+import Features from './components/Features';
+
 const App = () => {
   return (
     <React.Fragment>
       <NavBar />
       <Hero />
+      <Features />
     </React.Fragment>
   );
 };
