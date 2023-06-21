b107f38574aa81a6a00117a9c34a0753d82d2c84
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-06-21 09:46:01 -0500

    Added Hero to App

--- client/App.js
+++ client/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 
 //  Children components
 import NavBar from './components/NavBar';
+import Hero from './components/Hero';
 const App = () => {
   return (
     <React.Fragment>
       <NavBar />
+      <Hero />
     </React.Fragment>
   );
 };
