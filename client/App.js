3acbc8cdbe481b89eab0eea33cfb54d1dc7ced19
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-06-21 20:43:01 -0500

    Added Particles

--- client/App.js
+++ client/App.js
@@ -1,5 +1,7 @@
 //  Dependencies
-import React from 'react';
+import React, { useCallback } from 'react';
+import Particles from 'react-particles';
+import { loadFull } from 'tsparticles';
 
 //  Children components
 import NavBar from './components/NavBar';
@@ -8,12 +10,97 @@ import Features from './components/Features';
 import Team from './components/Team';
 
 const App = () => {
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
   return (
     <React.Fragment>
       <NavBar />
       <Hero />
       <Features />
       <Team />
+      <Particles
+        id='tsparticles'
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: '#c6d2dd',
+            },
+          },
+          fpsLimit: 200,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: 'push',
+              },
+              onHover: {
+                enable: true,
+                mode: 'repulse',
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: '#f8f7f2',
+            },
+            links: {
+              color: '#f8f7f2',
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: 'none',
+              enable: true,
+              outModes: {
+                default: 'bounce',
+              },
+              random: false,
+              speed: 1,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: 'circle',
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
     </React.Fragment>
   );
 };
